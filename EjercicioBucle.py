0f3ea5b8c52a8aa271efe8e8895eb9fc251ad463
Ka1999 <karenvane1024@gmail.com>
2024-09-27 13:30:05 -0500

    Ejercicios rápidos de PY

---
+++ EjercicioBucle.py
@@ -0,0 +1,4 @@
+palabra = input ('Ingrese una palabra: ')
+
+for i in range(10): #range es para saber cuantas veces se itere 
+    print(palabra)
---
+++ concatenar.py
@@ -0,0 +1,5 @@
+texto1 = input ('Ingresa tu palabra: ')
+texto2 = input ('Ingresa tu segunda palabra: ')
+texto3 = input ('Ingresa tu tercera palabra: ')
+
+print (texto1, texto2, texto3)
